05f9c4e268e7988a81eabc76a81a1a242efe800d
Raghuram Pulijala <rpulijala36@gmail.com>
2013-10-30 15:46:18 -0400

    changes

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -8,7 +8,7 @@ class UserNotifier < ActionMailer::Base
     @friend = user_friendship.friend
 
     mail to: @friend.email,
-         subject: "#{@user.first_name} wants to be friends on Treebook"
+         subject: "#{@user.first_name} wants to be friends on Friend Zone"
   end
 
   def friend_request_accepted(user_friendship_id)
